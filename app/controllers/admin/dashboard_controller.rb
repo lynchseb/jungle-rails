4db3044a62045768cc5083ac2dbd2eb77e8aace7
Sebastian <lynchseb@gmail.com>
2021-01-08 20:21:52 +0000

    implement basic auth to dashboard controller, implement category and product count to dashboard controller and views for admin

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,9 @@
 class Admin::DashboardController < ApplicationController
+
+  http_basic_authenticate_with name: ENV['ADMIN_USER'], password: ENV['ADMIN_PASS']
+
   def show
+    @productCount = Product.all.count
+    @categoryCount = Category.all.count
   end
 end
